df8056e03a1de8b42f88591e23f8ce2f2585b621
IreneScarpanti <127410709+IreneScarpanti@users.noreply.github.com>
2024-10-18 13:24:58 +0000

    EsempioCodice

--- prova.py
+++ prova.py
@@ -1,1 +1,16 @@
-print("Hello")
+#comment 
+from time import sleep
+import psutil
+def read_cpu_usage():
+    cpu_t =psutil.cpu_times()
+    urs_sp_cputime = cpu_t.user
+    idle_time = cpu_t.idle
+    return urs_sp_cputime, idle_time
+
+if __name__ == "__main__":
+    while True:
+        u_t, i_t = read_cpu_usage()
+        #print(str(u_t) +" " + str(i_t))
+        print ("user time: %.2f" % (u_t))
+        sleep(1)
+
